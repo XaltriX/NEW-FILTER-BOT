f39a9f890a3c5b75ad66fd48a8b9289cb119f298
XaltriX <129171425+XaltriX@users.noreply.github.com>
2026-09-10 14:17:21 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -90,7 +90,7 @@ async def start(client, message):
         m=await message.reply_sticker("CAACAgUAAxkBAAEKVaxlCWGs1Ri6ti45xliLiUeweCnu4AACBAADwSQxMYnlHW4Ls8gQMAQ") 
         await asyncio.sleep(1)
         await m.delete()
-                        await message.reply_photo(
+                await message.reply_photo(
             photo=random.choice(PICS),
             caption=script.START_TXT.format(
                 message.from_user.mention,
@@ -99,8 +99,6 @@ async def start(client, message):
                 temp.B_NAME
             )
         )
-        return
-        return
         return
     
     if AUTH_CHANNEL and not await is_subscribed(client, message):
