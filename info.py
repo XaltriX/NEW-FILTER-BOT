ac299985a5c9f4c06f82833a00e57ca9dfc3adbe
XaltriX <129171425+XaltriX@users.noreply.github.com>
2026-09-11 12:08:37 +0530

    Update info.py

--- info.py
+++ info.py
@@ -63,8 +63,8 @@ DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in enviro
 
 # MongoDB information
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Cluster0:Cluster0@cluster0.c07xkuf.mongodb.net/?")   # IF Multiple Database Is False Then Fill Only This Database Url.
-DATABASE_NAME = environ.get('DATABASE_NAME', "newbot")
-COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegraam_files')
+DATABASE_NAME = environ.get('DATABASE_NAME', "newboot")
+COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegraaam_files')
 
 MULTIPLE_DATABASE = bool(environ.get('MULTIPLE_DATABASE', False)) # Set True or False
 
